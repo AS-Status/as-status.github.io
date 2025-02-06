0bf27741d6356565b06b448ba6c361ef0e7e4a76
andyliu <justgps@gmail.com>
2025-02-06 14:43:25 +0800

    <docs>change error to warning

---
+++ js/lang.js
@@ -0,0 +1,47 @@
+
+   const translations = {
+     zh: {
+       title: "Academia Sinica 服務狀態 Dashboard",
+       connectUpdating: "連線中...",
+       lastUpdated: "最後更新: ",
+       statusOverview: "總體狀態",
+       statusLegend: "AVAILABLE: ✔️, UNAVAILABLE: ❌, ERROR: ⚠️",
+       services: ["中研院官方網站", "Email 服務", "google calendar", "google drive", "VPN"],
+       statuses: {
+         "AVAILABLE": { 
+           icon: "✔️", 
+           label: "正常" 
+         },
+         "UNAVAILABLE": { 
+           icon: "❌", 
+           label: "中斷" 
+         },
+         "ERROR": { 
+           icon: "⚠️", 
+           label: "警告" 
+         }
+       }
+     },
+     en: {
+       title: "Academia Sinica Service Status Dashboard",
+       connectUpdating: "Connecting...",
+       lastUpdated: "Last updated: ",
+       statusOverview: "Overall Status",
+       statusLegend: "AVAILABLE: ✔️, UNAVAILABLE: ❌, ERROR: ⚠️",
+       services: ["Academia Sinica Official Website", "Email Service", "Google Calendar", "Google Drive", "VPN"],
+       statuses: {
+         "AVAILABLE": { 
+           icon: "✔️", 
+           label: "Normal" 
+         },
+         "UNAVAILABLE": { 
+           icon: "❌", 
+           label: "Interrupted" 
+         },
+         "ERROR": { 
+           icon: "⚠️", 
+           label: "Warning" 
+         }
+       }
+     }
+   };
